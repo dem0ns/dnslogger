9aecd819f9b12d4969e6a37fdce2ef05f53ee881
Dem0ns <28812134+dem0ns@users.noreply.github.com>
2022-03-15 19:18:44 +0800

    测试用例更新

---
+++ main_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/miekg/dns"
+	"net/http"
+	"testing"
+	"time"
+)
+
+func TestMain(m *testing.M) {
+	go main()
+	time.Sleep(3)
+	_ = m.Run()
+}
+
+func TestDNS(t *testing.T) {
+	t.Run("req", func(t *testing.T) {
+		ns := "localhost:53"
+		c := dns.Client{}
+		m := dns.Msg{}
+		m.SetQuestion("dnslogger.local.", dns.TypeA)
+		r, _, err := c.Exchange(&m, ns)
+		checkErr(err)
+		for _, ans := range r.Answer {
+			if ans.(*dns.A).A.String() != config.DefaultIp {
+				t.Errorf("DNS解析错误.\n")
+			}
+		}
+	})
+	ApiBase := fmt.Sprintf("http://localhost:%d", config.HttpPort)
+	t.Run("queryLatest", func(t *testing.T) {
+		reqest, err := http.NewRequest("GET", fmt.Sprintf("%s/api/latest", ApiBase), nil)
+		if err != nil {
+			panic(err)
+		}
+		response, _ := (&http.Client{}).Do(reqest)
+		if response.StatusCode != 200 {
+			t.Errorf("API status code异常. %d\n", response.StatusCode)
+		}
+	})
+}
